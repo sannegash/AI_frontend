ceb52cf11f3092f9dcbd8ea05b00519e2e72c9b2
sannegash <sannegash@gmail.com>
2024-12-16 21:07:29 +0300

    added account managment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import FileClaim from "./pages/FileClaim"; 
+import FileClaim from "./pages/FileClaim";
+import AccountManagment from "./pages/AccountManagment";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/fileclaim" element={<FileClaim />} />
+        <Route path="/accountmanagment" element={<AccountManagment />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/AccountManagment.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FileClaim = () => {
+  return (
+    <div>
+      <h1>File a Claim</h1>
+      <p>Form to file a claim goes here.</p>
+    </div>
+  );
+};
+
+export default FileClaim;
