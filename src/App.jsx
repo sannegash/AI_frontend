2417839d4988970196f15c6e59e3e6cd08d28cb4
sannegash <sannegash@gmail.com>
2024-12-16 21:02:49 +0300

    fixed adding fileclaim to routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import FileClaim from "./pages/FileClaim"; 
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/fileclaim" element={<FileClaim />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/FileClaim.jsx
@@ -0,0 +1,13 @@
+// FileClaim.jsx
+import React from 'react';
+
+const FileClaim = () => {
+  return (
+    <div>
+      <h1>File a Claim</h1>
+      <p>Form to file a claim goes here.</p>
+    </div>
+  );
+};
+
+export default FileClaim;
